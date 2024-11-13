2db902b7d537ba23a12a20a6a762ca9ae73e2ef2
Nitish Gupta <nitishgupta830@gmail.com>
2024-11-14 00:51:49 +0530

    create pages and routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,25 @@
 import React from 'react'
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import SignIn from './pages/SignIn'
+import Dashboard from './pages/Dashboard'
+import Home from './pages/Home'
+import SignUp from './pages/SignUp'
+import About from './pages/About'
+import Projects from './pages/Projects'
 
 const App = () => {
   return (
-    <div>
-      Main
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element ={<Home/>}/>
+        <Route path='/dashboard' element ={<Dashboard/>}/>
+        <Route path='/signin' element ={<SignIn/>}/>
+        <Route path='/signup' element ={<SignUp/>}/>
+        <Route path='/about' element ={<About/>}/> 
+        <Route path='/projects' element ={<Projects/>}/> 
+      </Routes>
+    
+    </BrowserRouter>
   )
 }
 
---
+++ client/src/pages/SignIn.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const SignIn = () => {
+  return (
+    <div>
+      signin
+    </div>
+  )
+}
+
+export default SignIn
+  
